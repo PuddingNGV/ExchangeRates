9656be3f49497cfa1f6214e05c5ce3d3313b471e
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-11-25 19:56:30 +0300

    Added test data RecyclerView

--- app/src/main/java/com/app/exchangerates/MainScreenActivity.kt
+++ app/src/main/java/com/app/exchangerates/MainScreenActivity.kt
@@ -2,6 +2,9 @@ package com.app.exchangerates
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.app.exchangerates.databinding.ActivityMainScreenBinding
 
 class MainScreenActivity : AppCompatActivity() {
@@ -26,5 +29,33 @@ class MainScreenActivity : AppCompatActivity() {
             binding.includeConverter.textNameFirstCurrency.text = textNameSecond
             binding.includeConverter.editTextValFirstCurrency.text = editTextValSecond
         }
+
+        val data = mutableListOf(
+            Currency("AUD", 1, "Австралийский доллар", 40.1688),
+            Currency("GBP", 1, "Фунт стерлингов Соединенного королевства", 71.8791),
+            Currency("USD", 1, "Доллар США",  60.5043),
+            Currency("EUR", 1, "Евро", 62.285),
+            Currency("INR", 1, "Индийских рупий", 74.3886),
+            Currency("CAD", 1, "Канадский доллар", 45.1659),
+            Currency("KGS", 100, "Киргизских сомов", 71.66),
+            Currency("CNY", 10, "Китайских юаней", 84.3616),
+            Currency("NOK", 10, "Норвежских крон", 60.2075),
+            Currency("UZS", 10000, "Узбекских сумов", 53.9788),
+            Currency("AUD", 1, "Австралийский доллар", 40.1688),
+            Currency("GBP", 1, "Фунт стерлингов Соединенного королевства", 71.8791),
+            Currency("USD", 1, "Доллар США",  60.5043),
+            Currency("EUR", 1, "Евро", 62.285),
+            Currency("INR", 1, "Индийских рупий", 74.3886),
+            Currency("CAD", 1, "Канадский доллар", 45.1659),
+            Currency("KGS", 100, "Киргизских сомов", 71.66),
+            Currency("CNY", 10, "Китайских юаней", 84.3616),
+            Currency("NOK", 10, "Норвежских крон", 60.2075),
+            Currency("UZS", 10000, "Узбекских сумов", 53.9788),
+            Currency("KRW", 1000, "Вон Республики Корея", 44.7583)
+        )
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = RecyclerAdapter(data)
+
     }
 }
