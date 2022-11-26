d06bc363c39d7d0d1a7cfcb11bbdf566c3145087
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-11-26 21:03:50 +0300

    Added DAO

---
+++ app/src/main/java/com/app/exchangerates/data/local/CurrencyDao.kt
@@ -0,0 +1,38 @@
+package com.app.exchangerates.data.local
+
+import androidx.room.*
+import androidx.room.OnConflictStrategy
+import com.app.exchangerates.data.local.entity.CurrencyDbEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CurrencyDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(currencyList: List<CurrencyDbEntity>)
+
+    @Query("SELECT * FROM currency")
+    fun getAll(): Flow<List<CurrencyDbEntity>>
+
+    @Query("DELETE FROM currency")
+    suspend fun deleteAll()
+
+/*
+    @Query
+    ("SELECT * FROM rockets WHERE id = :id")
+    suspend fun getRocketInfo(id: String): List<RocketInfo>
+
+    @Transaction
+    @Query("SELECT * FROM rockets WHERE id = :id")
+    suspend fun getStageList(id: String): List<RocketWithStage>
+
+    @Transaction
+    @Query("SELECT * FROM rockets WHERE id = :id")
+    suspend fun getFlickrImageList(id: String): List<RocketWithFlickrImage>
+
+    @Transaction
+    @Query("SELECT * FROM rockets WHERE id = :id")
+    suspend fun getPayloadWeightList(id: String): List<RocketWithPayloadWeight>
+*/
+
+}
