32ed791d707ad147b8932d50cb0fb0e59e37d5af
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-11-27 18:03:11 +0300

    Renamed fields and name fun

--- app/src/main/java/com/app/exchangerates/data/DataProcessing.kt
+++ app/src/main/java/com/app/exchangerates/data/DataProcessing.kt
@@ -1,7 +1,7 @@
 package com.app.exchangerates.data
 
 import com.app.exchangerates.data.local.entity.CurrencyDbEntity
-import com.app.exchangerates.data.remote.entinity.Valute
+import com.app.exchangerates.data.remote.entinity.CurrencyData
 import com.app.exchangerates.domain.models.CurrencyModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -10,18 +10,17 @@ class DataProcessing {
     fun toCurrencyModel(listFlow: Flow<List<CurrencyDbEntity>>): Flow<List<CurrencyModel>> {
         val currencyInfoList: Flow<List<CurrencyModel>> = listFlow.map { list ->
             list.map { value ->
-                value.toRocketInfo()
+                value.toCurrencyModel()
             }
         }
         return currencyInfoList
     }
 
-    fun toCurrencyDbEntity(currencyResponseList: List<Valute>): List<CurrencyDbEntity> {
+    fun toCurrencyDbEntity(currencyData: List<CurrencyData>): List<CurrencyDbEntity> {
         val mEntityList = mutableListOf<CurrencyDbEntity>()
-        for (i in currencyResponseList.indices) {
-            mEntityList.add(currencyResponseList[i].aMD.toCurrencyDbEntity())
+        for (i in currencyData.indices) {
+            mEntityList.add(currencyData[i].toCurrencyDbEntity())
         }
         return java.util.List.copyOf(mEntityList)
-
     }
 }
