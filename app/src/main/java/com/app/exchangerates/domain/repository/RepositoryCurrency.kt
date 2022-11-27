be22cf26bad397851050bde7538b1163be66419a
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-11-27 17:56:49 +0300

    Changed return data

--- app/src/main/java/com/app/exchangerates/domain/repository/RepositoryCurrency.kt
+++ app/src/main/java/com/app/exchangerates/domain/repository/RepositoryCurrency.kt
@@ -1,11 +1,11 @@
 package com.app.exchangerates.domain.repository
 
-import com.app.exchangerates.data.remote.entinity.Valute
+import com.app.exchangerates.data.remote.entinity.CurrencyData
 import com.app.exchangerates.domain.models.CurrencyModel
 import com.app.exchangerates.until.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface RepositoryCurrency {
-    suspend fun getRemoteCurrency(): Valute
+    suspend fun getRemoteCurrency(): List<CurrencyData>
     fun getLocalCurrency(): Flow<Resource<List<CurrencyModel>>>
 }
