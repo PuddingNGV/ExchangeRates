0bafec9578c13770ca44fb54093cd858a9f51d52
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-11-27 17:56:27 +0300

    Changed return data

--- app/src/main/java/com/app/exchangerates/domain/usecase/GetDataCurrencyUseCase.kt
+++ app/src/main/java/com/app/exchangerates/domain/usecase/GetDataCurrencyUseCase.kt
@@ -1,6 +1,6 @@
 package com.app.exchangerates.domain.usecase
 
-import com.app.exchangerates.data.remote.entinity.Valute
+import com.app.exchangerates.data.remote.entinity.CurrencyData
 import com.app.exchangerates.domain.models.CurrencyModel
 import com.app.exchangerates.domain.repository.RepositoryCurrency
 import com.app.exchangerates.until.Resource
@@ -10,7 +10,7 @@ import kotlinx.coroutines.withContext
 
 class GetDataCurrencyUseCase(private val currencyRepository: RepositoryCurrency) {
 
-    suspend fun executeRemote(): Valute {
+    suspend fun executeRemote(): List<CurrencyData> {
         return withContext(Dispatchers.IO){
             currencyRepository.getRemoteCurrency()
         }
