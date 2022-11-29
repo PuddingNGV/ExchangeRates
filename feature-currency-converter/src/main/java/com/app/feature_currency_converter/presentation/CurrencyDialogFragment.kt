e7f2215afeb889712e80310931e15b2d41aad1df
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-11-29 21:43:25 +0300

    Deletet test data, added livedata observer

--- feature-currency-converter/src/main/java/com/app/feature_currency_converter/presentation/CurrencyDialogFragment.kt
+++ feature-currency-converter/src/main/java/com/app/feature_currency_converter/presentation/CurrencyDialogFragment.kt
@@ -6,21 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.feature_currency_converter.databinding.FragmentCurrencyBinding
-import com.app.feature_currency_converter.domain.models.CurrencyModel
+import com.app.feature_currency_converter.domain.models.CurrencyModelModule
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CurrencyDialogFragment : DialogFragment() {
     private var _binding:FragmentCurrencyBinding? = null
     private val binding get() = _binding!!
+    private val vm: CurrencyConverterViewModel by viewModels()
+    private lateinit var data:  List<CurrencyModelModule>
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,41 +32,22 @@ class CurrencyDialogFragment : DialogFragment() {
 
     override fun onStart() {
         super.onStart()
+        vm.currencyConverterTest.observe(this@CurrencyDialogFragment) { listData ->
+            listData.data.let {
+                if (it != null) {
+                    data = it
+                    binding.recycler.adapter = RecyclerAdapter(data)
+                }
+            }
+        }
         dialog?.window?.setLayout(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT)
-        val data = mutableListOf(
-            CurrencyModel("AUD", 1, "Австралийский доллар", 40.1688),
-            CurrencyModel("GBP", 1, "Фунт стерлингов Соединенного королевства", 71.8791),
-            CurrencyModel("USD", 1, "Доллар США",  60.5043),
-            CurrencyModel("EUR", 1, "Евро", 62.285),
-            CurrencyModel("INR", 1, "Индийских рупий", 74.3886),
-            CurrencyModel("CAD", 1, "Канадский доллар", 45.1659),
-            CurrencyModel("KGS", 100, "Киргизских сомов", 71.66),
-            CurrencyModel("CNY", 10, "Китайских юаней", 84.3616),
-            CurrencyModel("NOK", 10, "Норвежских крон", 60.2075),
-            CurrencyModel("UZS", 10000, "Узбекских сумов", 53.9788),
-            CurrencyModel("AUD", 1, "Австралийский доллар", 40.1688),
-            CurrencyModel("GBP", 1, "Фунт стерлингов Соединенного королевства", 71.8791),
-            CurrencyModel("USD", 1, "Доллар США",  60.5043),
-            CurrencyModel("EUR", 1, "Евро", 62.285),
-            CurrencyModel("INR", 1, "Индийских рупий", 74.3886),
-            CurrencyModel("CAD", 1, "Канадский доллар", 45.1659),
-            CurrencyModel("KGS", 100, "Киргизских сомов", 71.66),
-            CurrencyModel("CNY", 10, "Китайских юаней", 84.3616),
-            CurrencyModel("NOK", 10, "Норвежских крон", 60.2075),
-            CurrencyModel("UZS", 10000, "Узбекских сумов", 53.9788),
-            CurrencyModel("KRW", 1000, "Вон Республики Корея", 44.7583)
-        )
-        binding.recycler.adapter = RecyclerAdapter(data)
+
         binding.recycler.addItemDecoration(
             DividerItemDecoration(
                 context, LinearLayoutManager.VERTICAL
             )
         )
         binding.recycler.layoutManager = LinearLayoutManager(context)
-    }
-
-    override fun onResume() {
-        super.onResume()
     }
 
     override fun onDestroy() {
