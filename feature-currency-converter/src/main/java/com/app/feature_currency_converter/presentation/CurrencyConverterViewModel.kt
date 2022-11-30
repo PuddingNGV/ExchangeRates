9440d7a670dc84dda202d2685ac95f475b815485
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-11-30 16:50:35 +0300

    Added UseCase

--- feature-currency-converter/src/main/java/com/app/feature_currency_converter/presentation/CurrencyConverterViewModel.kt
+++ feature-currency-converter/src/main/java/com/app/feature_currency_converter/presentation/CurrencyConverterViewModel.kt
@@ -1,11 +1,19 @@
 package com.app.feature_currency_converter.presentation
 
-import androidx.lifecycle.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.app.feature_currency_converter.domain.models.CurrencyModelModule
 import com.app.feature_currency_converter.domain.usecase.GetLocalDataUseCase
+import com.app.feature_currency_converter.domain.usecase.SaveSelectionItemUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class CurrencyConverterViewModel @Inject constructor(getLocalDataUseCase: GetLocalDataUseCase) : ViewModel() {
-    val currencyConverterTest = getLocalDataUseCase.executeLocal().asLiveData()
+class CurrencyConverterViewModel @Inject constructor(private val getLocalDataUseCase: GetLocalDataUseCase,
+    private val saveSelectionItemUseCase: SaveSelectionItemUseCase
+) : ViewModel() {
+    val currencyConverterLiveData = getLocalDataUseCase.executeLocal().asLiveData()
+
+    fun selectedItem(currencyModelModule: CurrencyModelModule) = saveSelectionItemUseCase.execute(currencyModelModule)
+
 }
