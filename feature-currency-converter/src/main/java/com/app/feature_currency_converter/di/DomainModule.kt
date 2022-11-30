4c4a7cd184304e1311db57ef48a67ec798b10968
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-11-30 16:51:11 +0300

    Added provides new usecase

--- feature-currency-converter/src/main/java/com/app/feature_currency_converter/di/DomainModule.kt
+++ feature-currency-converter/src/main/java/com/app/feature_currency_converter/di/DomainModule.kt
@@ -2,18 +2,24 @@ package com.app.feature_currency_converter.di
 
 import com.app.feature_currency_converter.domain.repository.RepositoryCurrencyConverter
 import com.app.feature_currency_converter.domain.usecase.GetLocalDataUseCase
+import com.app.feature_currency_converter.domain.usecase.SaveSelectionItemUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 class DomainModule {
 
     @Provides
     fun provideGetSettingsUseCase(repositoryCurrencyConverter: RepositoryCurrencyConverter): GetLocalDataUseCase {
         return GetLocalDataUseCase(repositoryCurrencyConverter)
+    }
+
+    @Provides
+    fun provideSaveSelectionItemUseCase(repositoryCurrencyConverter: RepositoryCurrencyConverter): SaveSelectionItemUseCase {
+        return SaveSelectionItemUseCase(repositoryCurrencyConverter)
     }
 
 }
