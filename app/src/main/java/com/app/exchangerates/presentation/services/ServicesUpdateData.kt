846628678e6f588dee341d5a437cb9a8b11eebf3
PuddingNGV <51071186+PuddingNGV@users.noreply.github.com>
2022-12-08 19:00:16 +0300

    Added notification

--- app/src/main/java/com/app/exchangerates/presentation/services/ServicesUpdateData.kt
+++ app/src/main/java/com/app/exchangerates/presentation/services/ServicesUpdateData.kt
@@ -1,42 +1,50 @@
 package com.app.exchangerates.presentation.services
 
 import android.app.*
+import android.app.NotificationManager.IMPORTANCE_HIGH
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.os.IBinder
 import android.util.Log
 import android.os.PowerManager
 import android.os.SystemClock
+import androidx.core.app.NotificationCompat
+import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.asLiveData
+import com.app.exchangerates.R
 import com.app.exchangerates.domain.models.CurrencyModelApp
 import com.app.exchangerates.domain.repository.RepositoryCurrency
 import com.app.exchangerates.presentation.MainScreenActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.toList
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class ServicesUpdateData : Service() {
+class ServicesUpdateData : LifecycleService() {
     private var wakeLock: PowerManager.WakeLock? = null
     private var isServiceStarted = false
     @Inject
     lateinit var myRepository: RepositoryCurrency
+    private val notificationChannelId = "SERVICE_CHANNEL"
     private lateinit var currencyModel: List<CurrencyModelApp>
+    private val notificationId = 1
+    lateinit var notificationBuilder: NotificationCompat.Builder
 
     override fun onCreate() {
         super.onCreate()
         Log.i("Services","onCreate")
-        val notification = createNotification()
-        startForeground(1, notification)
+        val notification = createNotification().build()
+        startForeground(notificationId, notification)
     }
 
     override fun onBind(intent: Intent): IBinder? {
+        super.onBind(intent)
         Log.i("Services", "onBind")
         return null
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
         Log.i("Services","onStartCommand, startId: $startId")
         if (intent != null) {
             val action = intent.action
@@ -54,11 +62,11 @@ class ServicesUpdateData : Service() {
     override fun onTaskRemoved(rootIntent: Intent) {
         val restartServiceIntent = Intent(applicationContext, ServicesUpdateData::class.java).also {
             it.setPackage(packageName)
-        };
-        val restartServicePendingIntent: PendingIntent = PendingIntent.getService(this, 1, restartServiceIntent, PendingIntent.FLAG_ONE_SHOT);
-        applicationContext.getSystemService(Context.ALARM_SERVICE);
-        val alarmService: AlarmManager = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager;
-        alarmService.set(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + 1000, restartServicePendingIntent);
+        }
+        val restartServicePendingIntent: PendingIntent = PendingIntent.getService(this, 1, restartServiceIntent, PendingIntent.FLAG_ONE_SHOT)
+        applicationContext.getSystemService(Context.ALARM_SERVICE)
+        val alarmService: AlarmManager = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmService.set(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + 1000, restartServicePendingIntent)
     }
 
     override fun onDestroy() {
@@ -66,11 +74,15 @@ class ServicesUpdateData : Service() {
         Log.i("Services","onDestroy")
     }
 
+    @OptIn(DelicateCoroutinesApi::class)
     private fun startService() {
         if (isServiceStarted) return
         Log.i("Services", "startService")
         isServiceStarted = true
         setServiceState(this, ServiceState.STARTED)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel(notificationManager)
 
         // we need this lock so our service gets not affected by Doze Mode
         wakeLock =
@@ -80,23 +92,28 @@ class ServicesUpdateData : Service() {
                 }
             }
 
-        // we're starting a loop in a coroutine
-        GlobalScope.launch(Dispatchers.IO) {
-            while (isServiceStarted) {
-                launch(Dispatchers.IO) {
-                    Log.i("Services","Get Data")
-                    val repositoryData = myRepository.getLocalCurrency().toList()[0].data
-                    if (!repositoryData.isNullOrEmpty()) {
-                        currencyModel = repositoryData
-                    }
-                    Log.i("Services", currencyModel.toString())
-                }
-                delay(15000)
-            }
-            Log.i("Services","End of the loop for the service")
-        }
-    }
+        val data = myRepository.getLocalCurrency().asLiveData()
 
+        // we're starting a loop in a coroutine
+        GlobalScope.launch(Dispatchers.Main) {
+            while (isServiceStarted) {
+                launch(Dispatchers.Main) {
+                    data.observe(this@ServicesUpdateData) { currency ->
+                        if (!currency.data.isNullOrEmpty()) {
+                            currencyModel = currency.data.sortedBy { it.name }
+                            val randomData = currencyModel.random()
+                            val notification = notificationBuilder
+                                .setContentText("${randomData.name} - ${randomData.value}")
+                            notificationManager.notify(notificationId, notification.build())
+                            Log.i("Services", "is work")
+                        }
+                    }
+                }
+                delay(20000)
+            }
+        }
+            Log.i("Services","End of the loop for the service")
+    }
     private fun stopService() {
         Log.i("Services","stopService")
         try {
@@ -114,40 +131,25 @@ class ServicesUpdateData : Service() {
         setServiceState(this, ServiceState.STOPPED)
     }
 
-    //Полностью переписать создание уведомления
-    private fun createNotification(): Notification {
-        val notificationChannelId = "ENDLESS SERVICE CHANNEL"
-
-        // depending on the Android API that we're dealing with we will have
-        // to use a specific method to create the notification
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val channel = NotificationChannel(
-            notificationChannelId,
-            "Endless Service notifications channel",
-            NotificationManager.IMPORTANCE_HIGH
-        ).let {
-            it.description = "Endless Service channel"
-            it.enableLights(true)
-            it.lightColor = Color.RED
-            it
-        }
-        notificationManager.createNotificationChannel(channel)
-
+    private fun createNotification(): NotificationCompat.Builder {
         val pendingIntent: PendingIntent = Intent(this, MainScreenActivity::class.java).let { notificationIntent ->
             PendingIntent.getActivity(this, 0, notificationIntent, 0)
         }
 
-        val builder: Notification.Builder = Notification.Builder(
-            this,
-            notificationChannelId
-        )
-
-        return builder
-            .setContentTitle("Endless Service")
-            .setContentText("This is your favorite endless service working")
+        notificationBuilder = NotificationCompat.Builder(this, notificationChannelId)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_repeat)
+            .setContentText("UPDATE DATA")
+            //.setContentText("${random.charCode} - ${random.value}")
             .setContentIntent(pendingIntent)
-            .setTicker("Ticker text")
-            .setPriority(Notification.PRIORITY_HIGH) // for under android 26 compatibility
-            .build()
+
+        return notificationBuilder
+    }
+
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val notificationChannelName = "CURRENCY"
+        val channel = NotificationChannel(notificationChannelId, notificationChannelName, IMPORTANCE_HIGH)
+        notificationManager.createNotificationChannel(channel)
     }
 }
